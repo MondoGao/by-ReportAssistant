4e3a1ac47a65e131c36eed21e3afb1ae8ace3d16
杨雪娇 <504597216@qq.com>
2016-06-01 21:32:35 +0800

    修改前端模板

--- pc/tpl/build/.cache/index.js
+++ pc/tpl/build/.cache/index.js
@@ -1,11 +1,23 @@
-/*TMODJS:{"version":11,"md5":"dee58047453803d2b1f35feae7efa103"}*/
+/*TMODJS:{"version":25,"md5":"8ff3dedc41e6b745cd01a10366651e39"}*/
 template('index',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <img src="images/logo.png" class="logo"> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="javascript:;" class="upload-file">上传文件</a> </div> </div> </header> <section> ';
 $each(list,function($value,$index){
-$out+=' <div class="report"> <a class="icon"> <div class="report-logo"> <img src="';
-$out+=$escape($value.imgurl);
-$out+='" alt=""> </div> </a> <div class="report-intro"> <a>';
+$out+=' <div class="report"> <input type="hidden" value="';
+$out+=$escape($value.document_id);
+$out+='"> ';
+if($value.type === 'doc' || $value.type === 'docx'){
+$out+=' <a class="icon word"> <div class="report-logo"> <img src="images/word_logo.png" alt=""> </div> </a> ';
+}else if($value.type === 'ppt' || $value.type === 'pptx'){
+$out+=' <a class="icon ppt"> <div class="report-logo"> <img src="images/ppt_logo.png" alt=""> </div> </a> ';
+}else if($value.type === 'pdf'){
+$out+=' <a class="icon pdf"> <div class="report-logo"> <img src="images/pdf_logo.png" alt=""> </div> </a> ';
+}else if($value.type === 'zip'){
+$out+=' <a class="icon zip"> <div class="report-logo"> <img src="images/zip_logo.png" alt=""> </div> </a> ';
+}else if($value.type === 'rar'){
+$out+=' <a class="icon zip"> <div class="report-logo"> <img src="images/zip_logo.png" alt=""> </div> </a> ';
+}
+$out+=' <div class="report-intro"> <a>';
 $out+=$escape($value.title);
 $out+='</a> <p>';
 $out+=$escape($value.intro);
