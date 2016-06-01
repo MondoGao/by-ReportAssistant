f53f64f589387925d70705a49c7ed6d092db7469
杨雪娇 <504597216@qq.com>
2016-06-01 22:53:43 +0800

    试一下是否拉到数据

--- pc/scripts/disk/home.js
+++ pc/scripts/disk/home.js
@@ -1,17 +1,12 @@
-// var mouseclick = function(obj,color){
-// 	$(obj).mousedown(function(){
-// 		$(obj).css("background-color",color);
-// 		$(obj).css("color","#000");
-// 		console.log("mousedown");
-// 	})
-// 	$(obj).mouseup(function(){
-// 		$(obj).css("background-color","#282828");
-// 		$(obj).css("color","#fff");
-// 		console.log("mousedown");
-// 	})
-// }
-
-
-// mouseclick(".upload","#FFFFFF");
-// mouseclick(".report-logo","blue");
-
+window.onload = function () {
+	$.ajax({
+        type: "post",
+        url: "/main_page",     
+        success: function(data) {
+           	console.log(data.result.document_id);
+        },
+        error: function(data) {
+            document.write = $.parseJSON(data.responseText).error;
+    	},     
+    }) 
+}
