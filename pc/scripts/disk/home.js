9ae90541230752a64c94d2e5e7a5d96b7dd02682
杨雪娇 <504597216@qq.com>
2016-06-05 23:58:48 +0800

    .

--- pc/scripts/disk/home.js
+++ pc/scripts/disk/home.js
@@ -1,3 +1,4 @@
+//一进页面加载代码
 window.onload = function () {
 	$.ajax({
         type: "post",
@@ -7,6 +8,73 @@ window.onload = function () {
         },
         error: function(data) {
             document.write = $.parseJSON(data.responseText).error;
-    	},     
+    		},     
     }) 
 }
+
+//下一页或上一页
+function turnpage(){
+	var pageOn = 0;
+	for(var i=0;i<=$("li").length;i++){
+		if($("li").eq(i).hasClass("pageOn")){
+			pageOn = i;
+			console.log(pageOn);
+		}
+	}
+	$("#next").click(function(){
+		if(pageOn<=3){
+			pageOn = pageOn + 1;
+			$("li").eq(pageOn-1).removeClass("pageOn");
+			$("li").eq(pageOn).addClass("pageOn");
+		}
+		else{
+			return;
+		}
+		$.ajax({
+        	type: "post",
+        	url: "/main_page",     
+        	success: function(data) {
+           		console.log(data.result.document_id);
+        	},
+        	error: function(data) {
+	            document.write = $.parseJSON(data.responseText).error;
+    		},     
+    	}) 
+	})
+	$("#prev").click(function(){
+		if(pageOn>=1){
+			pageOn = pageOn - 1;
+			$("li").eq(pageOn+1).removeClass("pageOn");
+			$("li").eq(pageOn).addClass("pageOn");
+		}
+		else{
+			return;
+		}
+		$.ajax({
+        	type: "post",
+        	url: "/main_page",     
+        	success: function(data) {
+           		console.log(data.result.document_id);
+        	},
+        	error: function(data) {
+	            document.write = $.parseJSON(data.responseText).error;
+    		},     
+    	}) 
+	})
+}
+turnpage();
+
+//搜索
+$(".search-submit").click(function(){
+	$.ajax({
+       	type: "post",
+       	url: "/search",
+       	data:"",     
+       	success: function(data) {
+       		console.log(data.result.document_id);
+        },
+        error: function(data) {
+	        document.write = $.parseJSON(data.responseText).error;
+    	},     
+    }) 	
+})
--- pc/tpl/build/.cache/index.js
+++ pc/tpl/build/.cache/index.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":27,"md5":"af6642cccde9cc7950938bcffe4be819"}*/
+/*TMODJS:{"version":29,"md5":"83f3b0ce7e98d8914909b7de4d1ad7c6"}*/
 template('index',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <img src="images/logo.png" class="logo"> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="javascript:;" class="upload-file">上传文件</a> </div> </div> </header> <section> ';
@@ -27,6 +27,6 @@ $out+='</p> <span>';
 $out+=$escape($value.downNum);
 $out+='</span> </div> </div> ';
 });
-$out+=' </section> <div class="changePage clear"> <img src="images/prev.png" alt=""> <ul> <li class="pageOn"><div>1</div></li> <li><div>2</div></li> <li><div>3</div></li> <li><div>4</div></li> <li><div>5</div></li> </ul> <img src="images/next.png" alt=""> </div>';
+$out+=' </section> <div class="changePage clear"> <img src="images/prev.png" alt="" id="prev"> <ul id="page_num"> <li class="pageOn"><div>1</div></li> <li><div>2</div></li> <li><div>3</div></li> <li><div>4</div></li> <li><div>5</div></li> </ul> <img src="images/next.png" alt="" id="next"> </div>';
 return new String($out);
 });
