6403e303fa4f0cc426f7ba3753b8fea0aeeee6d1
杨雪娇 <504597216@qq.com>
2016-06-06 15:50:24 +0800

    .

--- pc/scripts/dist/report_home.js
+++ pc/scripts/dist/report_home.js
@@ -39,6 +39,11 @@ function turnpage(){
 			$("li").eq(pageOn-1).removeClass("pageOn");
 			$("li").eq(pageOn).addClass("pageOn"); 
 		}
+		/*
+			可翻页的数量不大 
+			超出的页面暂且没管
+			限定了页数
+		*/ 
 		// else{
 		// 	pageOn = pageOn + 1;
 		// 	var pageUL = document.getElementById("page_num");
@@ -56,6 +61,16 @@ function turnpage(){
 			$("li").eq(pageOn).addClass("pageOn");
 		}
 	})
+	for(var i=0;i<$("li").length;i++){
+		$("li").eq(i).click((function(i){
+			return function(){
+				$("li").eq(pageOn).removeClass("pageOn");
+				$("li").eq(i).addClass("pageOn");
+				pageOn = i;
+				getdata(pageOn+1);
+			}
+		})(i));
+	}
 }
 turnpage();
 
