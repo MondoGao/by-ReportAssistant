bbc590c37822615956ec6447125e47de5f185e6a
杨雪娇 <504597216@qq.com>
2016-06-06 10:56:52 +0800

    .

--- pc/tpl/build/.cache/index.js
+++ pc/tpl/build/.cache/index.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":33,"md5":"bcf7757068d25440e319fe12503dbbc4"}*/
+/*TMODJS:{"version":37,"md5":"0e3b153829a9bdbe9a9afc7820c27f68"}*/
 template('index',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <img src="images/logo.png" class="logo"> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="report_upload.html" class="upload-file" target="upload_window">上传文件</a> </div> </div> </header> <section> ';
@@ -22,10 +22,14 @@ $out+=' <a class="icon zip"> <div class="report-logo"> <img src="images/zip_logo
 $out+=' <div class="report-intro"> <a>';
 $out+=$escape($value.document_name);
 $out+='</a> <p>';
-$out+=$escape($value.desc);
-$out+='</p> <span>';
+$out+=$escape($value.grade);
+$out+='&nbsp;';
+$out+=$escape($value.institute);
+$out+='&nbsp;';
+$out+=$escape($value.class);
+$out+='</p> <span>已有';
 $out+=$escape($value.downloads);
-$out+='</span> </div> </div> ';
+$out+='人下载</span> </div> </div> ';
 });
 $out+=' </section> <div class="changePage clear"> <img src="images/prev.png" alt="" id="prev"> <ul id="page_num"> <li class="pageOn"><div>1</div></li> <li><div>2</div></li> <li><div>3</div></li> <li><div>4</div></li> <li><div>5</div></li> </ul> <img src="images/next.png" alt="" id="next"> </div>';
 return new String($out);
