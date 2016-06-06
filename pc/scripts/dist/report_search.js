f49bf2e7c8047982d90776e7893c4f3eb66318aa
杨雪娇 <504597216@qq.com>
2016-06-06 16:42:06 +0800

    搜索框拉数据

--- pc/scripts/dist/report_search.js
+++ pc/scripts/dist/report_search.js
@@ -1,6 +1,6 @@
 function firstshow(){
     var hrefPra = gethref();
-    var searchKey =  hrefPra[hrefPra.length-1];
+    var searchKey ='"'+hrefPra[hrefPra.length-1]+'"';
 	getdata(1,searchKey);
 }
 firstshow();
