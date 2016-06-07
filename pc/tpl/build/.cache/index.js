41de39d7aa9b6f9e2edcbc00ad96c567eb86f1b6
杨雪娇 <504597216@qq.com>
2016-06-07 21:54:55 +0800

    .

--- pc/tpl/build/.cache/index.js
+++ pc/tpl/build/.cache/index.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":76,"md5":"6e37317e28b4b2cf3ac17831d9e9fc76"}*/
+/*TMODJS:{"version":77,"md5":"73b01d444c4d91c989e5fdd5770c8f5c"}*/
 template('index',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <img src="images/logo.png" class="logo"> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="report_upload.html" class="upload-file" target="upload_window">上传文件</a> </div> </div> </header> <section> ';
@@ -27,7 +27,9 @@ $out+=' <a class="icon zip" href="report_detail.html?id=';
 $out+=$escape($value.document_id);
 $out+='" target="view_window"> <div class="report-logo"> <img src="images/zip_logo.png" alt=""> </div> </a> ';
 }
-$out+=' <div class="report-intro"> <a>';
+$out+=' <a class="report-intro" href="report_detail.html?id=';
+$out+=$escape($value.document_id);
+$out+='" target="view_window"> <a>';
 $out+=$escape($value.document_name);
 $out+='</a> <p>';
 $out+=$escape($value.grade);
@@ -37,7 +39,7 @@ $out+='&nbsp;';
 $out+=$escape($value.class);
 $out+='</p> <span>已有';
 $out+=$escape($value.downloads);
-$out+='人下载</span> </div> </div> ';
+$out+='人下载</span> </a> </div> ';
 });
 $out+=' </section> ';
 return new String($out);
