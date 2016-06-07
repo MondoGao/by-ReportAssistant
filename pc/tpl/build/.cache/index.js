570308701565f01a92316e2cb8deb8a6cf2e39d3
杨雪娇 <504597216@qq.com>
2016-06-07 19:53:58 +0800

    .

--- pc/tpl/build/.cache/index.js
+++ pc/tpl/build/.cache/index.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":53,"md5":"d3c9908c78750c0694238b2a210d606d"}*/
+/*TMODJS:{"version":55,"md5":"d3c9908c78750c0694238b2a210d606d"}*/
 template('index',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <img src="images/logo.png" class="logo"> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="report_upload.html" class="upload-file" target="upload_window">上传文件</a> </div> </div> </header> <section> ';
