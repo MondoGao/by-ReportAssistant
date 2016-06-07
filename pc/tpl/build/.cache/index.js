7604910cb0ed4e40d7ca3aac9e6a045f60b726b8
杨雪娇 <504597216@qq.com>
2016-06-07 22:07:11 +0800

    .

--- pc/tpl/build/.cache/index.js
+++ pc/tpl/build/.cache/index.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":77,"md5":"73b01d444c4d91c989e5fdd5770c8f5c"}*/
+/*TMODJS:{"version":80,"md5":"d6d3c96e3fd43d91a7dbc813ccafe662"}*/
 template('index',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <img src="images/logo.png" class="logo"> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="report_upload.html" class="upload-file" target="upload_window">上传文件</a> </div> </div> </header> <section> ';
@@ -29,9 +29,9 @@ $out+='" target="view_window"> <div class="report-logo"> <img src="images/zip_lo
 }
 $out+=' <a class="report-intro" href="report_detail.html?id=';
 $out+=$escape($value.document_id);
-$out+='" target="view_window"> <a>';
+$out+='" target="view_window"> <h3>';
 $out+=$escape($value.document_name);
-$out+='</a> <p>';
+$out+='</h3> <p>';
 $out+=$escape($value.grade);
 $out+='&nbsp;';
 $out+=$escape($value.institute);
