a155da0f5e0933fe1cf8a0f5d6a82fb181db3cc5
杨雪娇 <504597216@qq.com>
2016-06-07 19:49:14 +0800

    .

--- pc/tpl/build/.cache/index.js
+++ pc/tpl/build/.cache/index.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":49,"md5":"8ef9067fd7914b94bea97f0b7c00d8e9"}*/
+/*TMODJS:{"version":53,"md5":"d3c9908c78750c0694238b2a210d606d"}*/
 template('index',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <img src="images/logo.png" class="logo"> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="report_upload.html" class="upload-file" target="upload_window">上传文件</a> </div> </div> </header> <section> ';
@@ -9,7 +9,7 @@ $out+=' <a class="report" href="report_detail.html?id=';
 $out+=$escape($value.document_id);
 $out+='" target="view_window"> <input type="hidden" value="';
 $out+=$escape($value.document_id);
-$out+='"> <a class="icon word"> <div class="report-logo"> <img src="images/word_logo.png" alt=""> </div> </a> <div class="report-intro"> <a>';
+$out+='"> <div class="icon word"> <div class="report-logo"> <img src="images/word_logo.png" alt=""> </div> </div> <div class="report-intro"> <a>';
 $out+=$escape($value.document_name);
 $out+='</a> <p>';
 $out+=$escape($value.grade);
@@ -25,7 +25,7 @@ $out+=' <a class="report" href="report_detail.html?id=';
 $out+=$escape($value.document_id);
 $out+='" target="view_window"> <input type="hidden" value="';
 $out+=$escape($value.document_id);
-$out+='"> <a class="icon ppt" > <div class="report-logo"> <img src="images/ppt_logo.png" alt=""> </div> </a> <div class="report-intro"> <a>';
+$out+='"> <div class="icon ppt" > <div class="report-logo"> <img src="images/ppt_logo.png" alt=""> </div> </div> <div class="report-intro"> <a>';
 $out+=$escape($value.document_name);
 $out+='</a> <p>';
 $out+=$escape($value.grade);
@@ -41,7 +41,7 @@ $out+=' <a class="report" href="report_detail.html?id=';
 $out+=$escape($value.document_id);
 $out+='" target="view_window"> <input type="hidden" value="';
 $out+=$escape($value.document_id);
-$out+='"> <a class="icon pdf" > <div class="report-logo"> <img src="images/pdf_logo.png" alt=""> </div> </a> <div class="report-intro"> <a>';
+$out+='"> <div class="icon pdf" > <div class="report-logo"> <img src="images/pdf_logo.png" alt=""> </div> </div> <div class="report-intro"> <a>';
 $out+=$escape($value.document_name);
 $out+='</a> <p>';
 $out+=$escape($value.grade);
@@ -57,7 +57,7 @@ $out+=' <a class="report" href="report_detail.html?id=';
 $out+=$escape($value.document_id);
 $out+='" target="view_window"> <input type="hidden" value="';
 $out+=$escape($value.document_id);
-$out+='"> <a class="icon zip" > <div class="report-logo"> <img src="images/zip_logo.png" alt=""> </div> </a> <div class="report-intro"> <a>';
+$out+='"> <div class="icon zip" > <div class="report-logo"> <img src="images/zip_logo.png" alt=""> </div> </div> <div class="report-intro"> <a>';
 $out+=$escape($value.document_name);
 $out+='</a> <p>';
 $out+=$escape($value.grade);
@@ -73,7 +73,7 @@ $out+=' <a class="report" href="report_detail.html?id=';
 $out+=$escape($value.document_id);
 $out+='" target="view_window"> <input type="hidden" value="';
 $out+=$escape($value.document_id);
-$out+='"> <a class="icon zip" > <div class="report-logo"> <img src="images/zip_logo.png" alt=""> </div> </a> <div class="report-intro"> <a>';
+$out+='"> <div class="icon zip" > <div class="report-logo"> <img src="images/zip_logo.png" alt=""> </div> </div> <div class="report-intro"> <a>';
 $out+=$escape($value.document_name);
 $out+='</a> <p>';
 $out+=$escape($value.grade);
