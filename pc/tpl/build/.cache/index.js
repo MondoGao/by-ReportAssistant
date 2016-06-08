c6b7024f145a43e85a668de85f868aa290f6638a
杨雪娇 <504597216@qq.com>
2016-06-08 13:35:46 +0800

    .

--- pc/tpl/build/.cache/index.js
+++ pc/tpl/build/.cache/index.js
@@ -1,7 +1,7 @@
-/*TMODJS:{"version":83,"md5":"056b4ac580e02bd0146b53c7a29037f7"}*/
+/*TMODJS:{"version":84,"md5":"5b666fb1109052e4429596905b375c94"}*/
 template('index',function($data,$filename
 /**/) {
-'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <a href="report_home.html" target="home_window" class="logo"> <img src="images/logo.png"> </a> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="javascript:;" class="upload-file">上传文件</a> </div> </div> </header> <section> ';
+'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,list=$data.list,$value=$data.$value,$index=$data.$index,$escape=$utils.$escape,$out='';$out+='<header> <a href="report_home.html" target="home_window" class="logo"> <img src="images/logo.png"> </a> <div class="search-container"> <img src="images/header-bg.png"> <div> <div class="search"> <input type="text" id="search-input"> <a href="javascript:;" class="search-submit">搜索</a> </div> <a href="report_upload.html" class="upload-file" target="upload_window">上传文件</a> </div> </div> </header> <section> ';
 $each(list,function($value,$index){
 $out+=' <div class="report"> <input type="hidden" value="';
 $out+=$escape($value.document_id);
