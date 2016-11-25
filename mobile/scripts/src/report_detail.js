e7d7ef81a0ae4f5f16fe0f90da5b5631ad8df8a4
风之雪 <tiancaigaohua@163.com>
2016-11-25 09:34:08 +0800

    * change preview's position * fit small height's device * optimize detail's js * update readme.md

--- mobile/scripts/src/report_detail.js
+++ mobile/scripts/src/report_detail.js
@@ -4,38 +4,6 @@ var reportThumb = require('../../tpl/report-preview-thumb.tpl');
 var reportMiss = require('../../tpl/report-preview-miss.tpl');
 var Reminder = require('./reminder');
 
-// var test_data = {
-//     document_id: 2123,
-//     document_name: 'C课设 报告&代码',
-//     uploader: '小宝上传',
-//     institude: '13级计算机学院',
-//     class: 'C课程设计',
-//     downloads: 123,
-//     comment: 'hello',
-//     type: 'doc',
-//     preview: ['images/report-preview.png', 'images/report-preview.png', 'images/report-preview.png']
-// };
-//
-// var test_data2 = {
-//     result: {
-//         document_name: 'C课设 报告&代码',
-//         uploader: '小宝上传',
-//         institude: '13级计算机学院',
-//         class: 'C课程设计',
-//         downloads: 123,
-//         comment: 'hello',
-//         type: 'doc',
-//         preview: ['images/report-preview.png', 'images/report-preview.png', 'images/report-preview.png']
-//     }
-// };
-
-// setTimeout(function () {
-    // $('.report-container').append(reportThumb(test_data2));
-    // $('.report-preview-container').append(reportPreview(test_data));
-    // $('.report-download').append(reportDownload(test_data));
-    // $('.loading-icon').addClass('hide');
-// }, 1000);
-
 var reminder = new Reminder();
 reminder.init();
 
@@ -46,13 +14,10 @@ if (searchUrl.indexOf('?') !== -1) {
 }
 
 $('.download-btn').on('tap', function () {
-    var document_id = search.split('=')[1];
-    var url = '/download/' + document_id;
     $('.report-download').addClass('show-download-info');
-    $.ajax({
-        url: url,
-        type: 'GET'
-    });
+    if(document.body.scrollHeight > document.body.clientHeight) {
+        document.body.className = "scrollable";
+    }
 });
 
 if (search !== '') {
@@ -62,24 +27,18 @@ if (search !== '') {
         type: 'GET'
     }).done(function (data) {
         var str = '我在"报告菌"上找到了' + data.result.document_name + ',这下不用担心了';
-
-        // data.result.preview = "http://localhost:3000/mobi/6633.html";
-
         $('title').text(str);
+
+        data.result.preview = "http://localhost:3000/mobi/6633.html";
+
         $('.report-container').append(reportThumb(data));
-        // if (data.result.preview.length === 0) {
-        //     $('.report-preview-container').append(reportMiss());
-        // } else {
-        //     $('.report-preview-container').append(reportPreview(data.result));
-        // }
+        $('.report-download').append(reportDownload(data.result));
         if (!data.result.preview) {
             $('.report-preview-container').append(reportMiss());
-            $('.report-download').append(reportDownload(data.result));
             $('.report-type-info').hide();
             $('.loading-icon').addClass('hide');
         } else {
             $('.report-preview-container').append(reportPreview(data.result));
-            $('.report-download').append(reportDownload(data.result));
             $('#ifr-container').on('load', function () {
                 var ifr = document.getElementById('ifr-container'),
                     ifrDoc = ifr.contentDocument || ifr.contentWindow.document,
@@ -89,15 +48,18 @@ if (search !== '') {
                     pf = ifrDoc.getElementById('pf1');
                 var scale = container.offsetWidth / pf.offsetWidth;
 
-                var u = navigator.userAgent;
-                var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
-                var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+                // var u = navigator.userAgent;
+                // var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+                // var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+                //
+                // var scaleTxt = "body{margin: 0;}div[id^='pf']{-webkit-transform: scaleX(" + scale + ");transform:scaleX(" + scale + ");";
+                // if (isAndroid) {
+                //     scaleTxt += "-webkit-transform-origin: 0 0;transform-origin: 0 0";
+                // }
+                // scaleTxt += "}";
 
-                var scaleTxt = "div[id^='pf']{-webkit-transform: scaleX(" + scale + ");transform:scaleX(" + scale + ");";
-                if (isAndroid) {
-                    scaleTxt += "-webkit-transform-origin: 0 0;transform-origin: 0 0";
-                }
-                scaleTxt += "}";
+                var scaleTxt = "div[id^='pf']{width:" + container.offsetWidth + "px;height:" + pf.offsetHeight * scale + "px}.pc{-webkit-transform: scale(" + scale + ");transform:scale(" + scale + ");}"
+
                 var touchTxt = "#page-container{-webkit-overflow-scrolling: touch;}";
                 ifrStyle.setAttribute('type', 'text/css');
                 ifrHead.appendChild(ifrStyle);
@@ -108,20 +70,24 @@ if (search !== '') {
                         el.classList[fun]('hide');
                     });
                 }
-                $('#ifr-container').contents().find('#page-container').on('scroll',(function() {
+                $(container).on('scroll',(function() {
                     var _scrollTop = 0;
                     var deltaTop;
                     return function(e) {
                         deltaTop = e.target.scrollTop - _scrollTop;
                         _scrollTop = e.target.scrollTop;
                         // console.log(deltaTop);
-                        if(deltaTop > 0) {
+                        if(deltaTop > 4) {
                             toggleBtn('add');
-                        } else {
+                        } else if(deltaTop < 0) {
                             toggleBtn('remove');
                         }
                     };
                 })());
+
+                if(document.body.scrollHeight > document.body.clientHeight) {
+                    document.body.className = "scrollable";
+                }
 
                 $(ifr).off('load');
                 $('.loading-icon').addClass('hide');
@@ -137,18 +103,3 @@ if (search !== '') {
 $('#btn-fullscreen').on('touchstart', function() {
     $('.report-preview-container').toggleClass('fullscreen');
 });
-
-// $('#ifr-container').on('load', function () {
-//     var ifr = document.getElementById('ifr-container'),
-//         ifrDoc = ifr.contentDocument || ifr.contentWindow.document,
-//         ifrHead = ifrDoc.getElementsByTagName('head')[0],
-//         ifrStyle = document.createElement('style');
-//     var container = ifrDoc.getElementById('page-container'),
-//         pf = ifrDoc.getElementById('pf1');
-//     var scale = container.offsetWidth / pf.offsetWidth;
-//     var text = "div[id^='pf']{-webkit-transform: scaleX(" + scale + ");transform:scaleX(" + scale + ");-webkit-transform-origin: 0 100%;transform-origin: 0 100%}";
-//     ifrStyle.setAttribute('type', 'text/css');
-//     ifrHead.appendChild(ifrStyle);
-//     ifrStyle.innerHTML = text;
-//     $(ifr).off('load');
-// });
